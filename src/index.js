51c7bca16e015f5c0e55f9611dec8c8b4c8b6d86
K ANIL KUMAR <kavali.anil.dev@email.com>
2025-08-08 14:05:30 +0530

    Your commit message describing new changes

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ import Calendar from './Components/Calendar';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-   
     <Calendar/>
   </React.StrictMode>
 );
@@ -15,4 +14,5 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 
